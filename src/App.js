64b8f6530c7a87486a689e220120c230df5d65c4
Samuel <sam.davidsson86@outlook.com>
2019-04-04 22:38:26 +0200

    Init commit just a quick test of hooks

---
+++ src/App.js
@@ -0,0 +1,45 @@
+import React, { useState } from "react";
+import "./App.css";
+import Form from "./Form";
+
+
+export default () => {
+  const [todos, setTodos] = useState([]);
+
+  const toggleComplete = i =>
+    setTodos(
+      todos.map(
+        (todo, k) =>
+          k === i
+            ? {
+                ...todo,
+                complete: !todo.complete
+              }
+            : todo
+      )
+    );
+
+  return (
+    <div className="App">
+      <Form
+        onSubmit={text => setTodos([{ text, complete: false }, ...todos])}
+      />
+      <div>
+        {todos.map(({ text, complete }, i) => (
+          <div
+            key={text}
+            onClick={() => toggleComplete(i)}
+            style={{
+              textDecoration: complete ? "line-through" : ""
+            }}
+          >
+            {text}
+          </div>
+        ))}
+      </div>
+      <button onClick={() => setTodos([])}>reset</button>
+    </div>
+  );
+};
+
+
---
+++ src/Form.js
@@ -0,0 +1,22 @@
+import React, { useState } from "react";
+
+const userInputValue = (initalValue) => {
+    const [value, setValue] = useState(initalValue);
+    return {
+        value,
+        onChange: e => setValue(e.target.value),
+        resetValue: () => setValue("")
+    };
+};
+export default ({onSubmit}) => {
+    const {resetValue, ...text} = userInputValue("");
+    return (
+        <form onSubmit={e => {
+            e.preventDefault();
+            onSubmit(text.value);
+            resetValue();
+        }}>
+            <input {...text} />
+        </form>
+    )
+}
